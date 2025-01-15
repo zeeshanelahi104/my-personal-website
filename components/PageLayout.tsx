e6ab46c6420fb199015281524662440bce8b753b
Memoona-tech <memoonas2024@gmail.com>
2025-01-15 05:06:36 +0500

    animation duration(content appear) change

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,29 +1,30 @@
 "use client";
-import React from 'react'
-import  Container  from './container'
-import {motion} from 'motion/react'
-import {cn} from '@/lib/utils'
-interface Props{
-    children: React.ReactNode,
-    className?: string
+import React from "react";
+import Container from "./container";
+import { motion } from "motion/react";
+import { cn } from "@/lib/utils";
+interface Props {
+  children: React.ReactNode;
+  className?: string;
 }
-const PageLayout = ({children,className}:Props) => {
+const PageLayout = ({ children, className }: Props) => {
   return (
-    <Container className={cn('w-full ',className)}>
-        <motion.div initial={{opacity:0}}
+    <Container className={cn("w-full ", className)}>
+      <motion.div
+        initial={{ opacity: 0 }}
         animate={{
-            opacity:1,
-            transition:{
-                delay:2.4,
-                duration:0.2,
-                ease:'easeIn'
-            }
+          opacity: 1,
+          transition: {
+            delay: 1.4,
+            duration: 0.2,
+            ease: "easeIn",
+          },
         }}
-        >
-            {children}
-        </motion.div>
+      >
+        {children}
+      </motion.div>
     </Container>
-  )
-}
+  );
+};
 
-export default PageLayout
+export default PageLayout;
